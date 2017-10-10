2b94079295198a1954263f437c9e86f2f51fc2cc
OpenSSL <y15948208685@gmail.com>
2017-10-10 18:50:29 +0800

    修复自定义Chrome path不生效的问题

--- test/download-npm.js
+++ test/download-npm.js
@@ -1,7 +1,7 @@
 let DDP = require('../lib/download.js')
 
 let downloadNPM = async () => {
-  let html = await DDP.downloadPage('https://www.npmjs.com/', `/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary`)
+  let html = await DDP.downloadPage('https://www.npmjs.com/')
   console.log('NPM HTML is:', html)
 }
 
