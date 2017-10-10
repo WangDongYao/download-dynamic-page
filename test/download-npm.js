88f2252e10255c7c92400eb97e66c25a5efd0fd7
OpenSSL <y15948208685@gmail.com>
2017-10-10 19:57:40 +0800

    支持centos7，重新设计下载

--- test/download-npm.js
+++ test/download-npm.js
@@ -1,7 +1,7 @@
 let DDP = require('../lib/download.js')
 
 let downloadNPM = async () => {
-  let html = await DDP.downloadPage('https://www.npmjs.com/')
+  let html = await DDP.downloadPage('https://www.npm.com/')
   console.log('NPM HTML is:', html)
 }
 
